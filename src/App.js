e6bb630c6651e57aaf007d880862f63a02d3a6dd
austin kwinta <arkwinta@gmail.com>
2021-10-13 19:24:48 -0400

    call the function in app and pass props down to the component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import logo from './logo.svg';
 import './App.css';
 import { Map } from './map'
 
 function App() {
+
+  const [covidData, setCovidData] = useState({})
+
+  async function getData() {
+    try {
+      const response = await axios.get("https://disease.sh/v3/covid-19/historical/all?lastDays=all");
+      const { data } = response
+      setCovidData(data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
-      <Map />
+      <Map covidData={covidData} />
     </div>
   );
 }
--- src/map.js
+++ src/map.js
@@ -1,35 +1,18 @@
 import { useState, useEffect } from 'react'
 import { getHistoricalCovidData } from './API'
+import axios from 'axios';
 
-export function Map() {
-    const [covidData, setCovidData] = useState({})
-
-    async function getData() {
-        try {
-            const covid = await getHistoricalCovidData();
-            console.log(covid)
-            setCovidData(covid)
-            console.log(covidData)
-        } catch (e) {
-            console.log(e)
-        }
-        console.log(covidData)
-    }
-
-
-    useEffect(() => {
-        getData()
-    }, [])
-
+export function Map(props) {
+    console.log(props)
 
     return (
         <div>
-            {/* <div>
-                Total Number of Covid Cases today: {covidData.cases['10/12/21']}
+            <div>
+                Total Number of Covid Cases today: {props.covidData.cases['10/12/21']}
             </div>
             <div>
-                Total Number of Covid deaths today: {covidData.deaths['10/12/21']}
-            </div> */}
+                Total Number of Covid deaths today: {props.covidData.deaths['10/12/21']}
+            </div>
         </div>
     )
 }
